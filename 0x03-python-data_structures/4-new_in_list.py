92a4541615f1c108194345bb300ad15eb5892633
cyrile450@gmail.com <cyrile450@gmail.com>
2022-12-04 23:13:52 +0000

    new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    i = 0
     new_list = []
     if idx < 0 or idx > len(my_list):
         return None
@@ -7,6 +8,8 @@ def new_in_list(my_list, idx, element):
     for e in my_list:
         if i is idx:
             e = element
+        else:
+            i += 1
         new_list.append(e)
 
     return new_list
