9e0026d55e02a7c79731bed1bdfe3d23444e76af
Myles2005 <cyrile450@gmail.com>
2023-02-15 03:54:35 -0800

    list all databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,4 @@
---Welcome to my first sql file :)
---This script prints all databases in your SQL server
+-- Welcome to my first sql file
+-- This script prints all databases in your SQL server
 
-mysql -u user -p
 SHOW DATABASES;
