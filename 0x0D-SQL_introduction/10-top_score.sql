39fc17aaf8b3c6fc2b79c4141a15292577b64e75
Myles2005 <cyrile450@gmail.com>
2023-02-17 05:09:05 -0800

    top scores shown

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
 -- Print out the name and score rows from second_table
-SELECT `score`, `name` FROM `second_table`;
+-- In the descending order of the score's value
+SELECT `score`, `name` FROM `second_table`
+ORDER BY `score` DESC;
