a594884b73b2057574a257189e00f710422eaf32
Myles2005 <cyrile450@gmail.com>
2023-03-14 09:20:06 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,9 +7,14 @@ const args = process.argv;
 let max = 0;
 let i;
 
-for (i = 2; i <= args.length; i++) {
-  if (max < parseInt(args[i])) {
-    max = parseInt(args[i]);
+if (args.length > 3) {
+  for (i = 2; i <= args.length; i++) {
+    if (max < parseInt(args[i])) {
+      max = parseInt(args[i]);
+    }
   }
+}
+else {
+  max = 0;
 }
 console.log(max);
