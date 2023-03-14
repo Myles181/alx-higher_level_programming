443f1740b270458fcac9828e37ef6d094bf85056
Myles2005 <cyrile450@gmail.com>
2023-03-14 00:42:47 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const size = parseInt(args[2]);
+let i, j, row;
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < size; i++) {
+    row = ''
+    for (j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
