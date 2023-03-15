c940a5bc193c12444eb12a69d366e8178831e073
Myles2005 <cyrile450@gmail.com>
2023-03-15 14:50:12 +0000

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,13 +2,9 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0) {
-      this.width = undefined;
-    } else if (h <= 0) {
-      this.height = undefined;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
-}
+
 module.exports = Rectangle;
