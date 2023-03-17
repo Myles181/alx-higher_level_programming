f78eb18f9f8579bf60f849a9ca43745e283695a0
Myles2005 <cyrile450@gmail.com>
2023-03-17 07:22:33 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """ Connect to sql server """
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3]
+            )
+
+    """ Create a cursor object """
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    db = cursor.fetchall()
+    for i in range(len(db)):
+        print(db[i])
+
+    cursor.close()
