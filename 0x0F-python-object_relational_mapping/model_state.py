fd1a37c9f7f6d85a89bfce7b0b520ebacb5e3e96
Myles2005 <cyrile450@gmail.com>
2023-03-19 09:06:54 -0700

    correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,11 +9,12 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
     """ Link to table """
     __tablename__ = "states"
 
     """ Identify table's column """
-    id = Column(Integer, autoincrement = True,
-                primary_key = True, nullable = True)
-    name = Column(String(128), nullable = True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=True)
+    name = Column(String(128), nullable=True)
