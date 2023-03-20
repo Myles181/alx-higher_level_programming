a44c9e40b38c14904013ad0656dfb6b4c7b5060b
Myles2005 <cyrile450@gmail.com>
2023-03-19 18:27:02 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
+"""takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
-"""
-    List all rows of states in ascending order by ID
-    Where name column is specified
-"""
-import MySQLdb
-import sys
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
-            host="localhost", port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-            )
-    name = sys.argv[4]
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states\
-                WHERE name='{:s}' ORDER BY id ASC".format(name))
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
